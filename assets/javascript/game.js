b7d3eb2411deee662920e25b4c8ad3b7a9c83851
Charles Prestia <Charles Prestia>
2016-08-06 17:41:49 -0400

    Sorry for the poor submission on this -- I spent an absurd amount of time thinking through problems incorrectly and being sick the past few days.  Nowhere near the quality fo work I wanted to hand in, but I was starting to get on a roll for a while there and am looking forward to actually finishing this progam up fully after the deadline -- I want a rad functioning hangman gamegit add -A

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,52 +4,64 @@ window.onload = function() {
 var band = [
 	{
 		'name': 'OPERATION_IVY',
-		'mp3File': "assets/songs/Artificial Life - OPERATION IVY.mp3"
+		'mp3File': "assets/songs/Artificial Life - OPERATION IVY.mp3",
+		'placeholder': "_____________"
 
 	},
 	{
 		'name': 'BLACK_FLAG',
-		'mp3File': "assets/songs/Black Flag - Rise Above.mp3"
+		'mp3File': "assets/songs/Black Flag - Rise Above.mp3",
+		'placeholder': "__________"
 	},
 	{
 		'name': 'DESCENDENTS',
-		'mp3File': "assets/songs/Descendents - Suburban Home.mp3"
+		'mp3File': "assets/songs/Descendents - Suburban Home.mp3",
+		'placeholder': "___________"
 	},
 	{
 		'name': 'FUGAZI',
-		'mp3File': "assets/songs/Fugazi - Blueprint.mp3"
+		'mp3File': "assets/songs/Fugazi - Blueprint.mp3",
+		'placeholder': "______"
 	},
 	{
 		'name': 'MINOR_THREAT',
-		'mp3File': "assets/songs/Minor Threat, Minor Threat.mp3"
+		'mp3File': "assets/songs/Minor Threat, Minor Threat.mp3",
+		'placeholder': "____________"
 	},
 	{
 		'name': 'MISFITS',
-		'mp3File': "assets/songs/Misfits- Skulls.mp3"
+		'mp3File': "assets/songs/Misfits- Skulls.mp3",
+		'placeholder': "_______"
 	},
 	{
 		'name': 'OPERATION_IVY',
-		'mp3File': "assets/songs/Operation Ivy - Caution.mp3"
+		'mp3File': "assets/songs/Operation Ivy - Caution.mp3",
+		'placeholder': "_____________"
 	},
 	{
 		'name': 'RAMONES',
-		'mp3File': "assets/songs/Ramones - Rockaway Beach.mp3"
+		'mp3File': "assets/songs/Ramones - Rockaway Beach.mp3",
+		'placeholder': "_______"
 	},
 	{
 		'name': 'THE_CLASH',
-		'mp3File': "assets/songs/THE CLASH-JANIE JONES.mp3"
+		'mp3File': "assets/songs/THE CLASH-JANIE JONES.mp3",
+		'placeholder': "_________"
 	},
 	{
 		'name': 'THE_DEAD_KENNEDYS',
-		'mp3File': "assets/songs/The Dead Kennedys - Holiday in Cambodia.mp3"
+		'mp3File': "assets/songs/The Dead Kennedys - Holiday in Cambodia.mp3",
+		'placeholder': "_________________"
 	},
 	{
 		'name': 'THE_SEX_PISTOLS',
-		'mp3File': "assets/songs/the sex pistols bodies cd version.mp3"
+		'mp3File': "assets/songs/the sex pistols bodies cd version.mp3",
+		'placeholder': "_______________"
 	},
 	{
 		'name': 'YOUTH_OF_TODAY',
-		'mp3File': "assets/songs/Youth of Today - One Family.mp3"
+		'mp3File': "assets/songs/Youth of Today - One Family.mp3",
+		'placeholder': "______________"
 	}];
 
 // Declaring game variables
@@ -59,15 +71,106 @@ var wrongGuesses = [];
 var totalRounds = 0;
 var currentBand = "";
 var guessesRemaining = 10;
+var step = "beginGame"
 
-// Bootstrap 'Alerts'
-function youLose() {
-	<div class="alert alert-info">
-        <a href="index.html" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-		<strong> Sorry Chump </strong>
-		<br>
-		You're about as punk as punk as Walmart the day after Thanksgiving
-	</div>
+//switch case controlling gameflow
+document.onkeyup = function(event) {
+	userGuess = String.fromCharCode(event.keyCode).toUpperCase();
+
+	switch(step) {
+
+		case "endGame":
+			break;
+
+		case "beginGame":
+			songSetUp();
+			songSelection();
+
+
+		case "nextBandRound":
+	}
 }
+
+
+//Choosing a random song to play the game with
+function songSelection() {
+	var ranSongIndex = math.floor(Math.random()* band.length +1);
+	var currentBand = band.indexOF(ranSongIndex);
+	currentBand.mp3File.play();
+}
+
+
+// Game Initiation starts from default switch case
+function songSetUp () {
+	document.getElementById("wC").innerHTML = userWins;
+	document.getElementById("rC").innerHTML = totalRounds;
+	document.getElementById("userAlert").innerHTML = "<p>WHAT BAND IS THIS?</p>";
+	document.getElementById("guessRemain").innerHTML = guessesRemaining;
+}
+
+
+// Updating the current progress of the user's guesses
+function nameGuessUpdate() {
+	if(!endGame){
+		
+		if () {
+			wrongGuesses.push(userGuess);
+			for(var i=0, i < currentBandName.length; i++) {
+				if (currentBand[i] === userGuess) {
+					currentBandName[i] = userGuess;
+					remainingLetters = remainingLetters -1;
+				}
+			}
+		}
+		currentGuessStatus = currentBandName.join(" ");
+		document.getElementById("currentGuessStatus").innerHTML = "<h2>" + currentGuessStatus + "</h2>";
+
+		if (remainingLetters <= 0) {
+			userWins++;
+			step = "nextBandRound";
+			document.getElementById("wC").innerHTML = wC;
+		}
+	}
+}
+
+document.onkeyup = function(event) {
+	userGuess = String.fromCharCode(event.keyCode).toUpperCase();
+
+	switch(step) {
+
+		case "endGame":
+			break;
+
+		case "beginGame":
+
+		case "nextBandRound":
+	}
+}
+
+
+
+
+
+
+
+
+		document.getElementById("userAlert").innerHTML = "<p> PRESS ANY KEY TO START<p><br><p>YOU MAY ENTER $ AT ANY TIME TO QUIT.</p>";
+		document.getElementById("wC").innerHTML = userWins;
+		document.getElementById("rC").innerHTML = totalRounds;
+		document.getElementById("userAlert").innerHTML
+		document.getElementById("").innerHTML
+		songSelector;
+
+
+
+		document.getElementById("userAlert").innerHTML
+		document.getElementById("userAlert").innerHTML
+		document.getElementById("userAlert").innerHTML
+		document.getElementById("userAlert").innerHTML
+
+	}
+
+};
+
 
 
